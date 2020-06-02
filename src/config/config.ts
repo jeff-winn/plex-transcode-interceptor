846f0167b5bc94b22f3928beb5ed7b69f6b42ff2
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2020-06-02 19:45:02 -0400

    Updating config values.

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,9 +2,6 @@
 export interface Config { 
     /** Gets the transcoder url. */
     getTranscoderUrl(): string | undefined;
-
-    /** Gets a value identifying whether the instance is running in a production configuration. */
-    isProduction(): boolean;
 };
 
 export class ConfigImpl implements Config {
@@ -12,14 +9,6 @@ export class ConfigImpl implements Config {
     }
 
     public getTranscoderUrl(): string | undefined {
-        if (!this.isProduction()) {
-            return "http://localhost:34800/api/transcode";            
-        }
-
-        return this.environment.TRANSCODER_URL;
-    }
-
-    public isProduction(): boolean {
-        return false;
+        return this.environment.TRANSCODER_URL || "http://localhost:34800/api/transcode";
     }
 }
